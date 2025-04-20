c46e61831684c719408a55a4c013c1a2ecc53645
DHRUVRAJSINH CHUDASAMA <chudasamadhruvraj8@gmail.com>
2025-04-20 10:16:28 +0530

    Update PRACTICAL2.2.cpp

--- PRACTICAL2.2.cpp
+++ PRACTICAL2.2.cpp
@@ -9,16 +9,13 @@ using namespace std;
      string name;
      int marks[3];
 public:
-
+    
      student()
      {
      rollnumber=0;
      name="default name";
      marks[0]=marks[1]=marks[2]=0;
      }
-
-
-
      student(int r,string n,int m1,int m2,int m3)
      {
          rollnumber=r;
@@ -26,22 +23,11 @@ public:
          marks[0]=m1;
          marks[1]=m2;
          marks[2]=m3;
-
-
      }
-
-
-
-
      double cal_avg()
      {
          return (marks[0]+marks[1]+marks[2])/3.0;
      }
-
-
-
-
-
      void display()
 
      {
@@ -51,22 +37,13 @@ public:
 
      }
  };
-
-
  int main()
  {
      student s[3];
-
-
      for(int i=0; i<3 ; i++)
      {
-
-
      int roll,m1,m2,m3;
      string name;
-
-
-
       cout<<"Enter details for student "<<i+1<<endl;
       cout<<"enter roll number :";
       cin>>roll;
@@ -74,21 +51,12 @@ public:
       cin>>name;
       cout<<"enter marks of three subjects :";
       cin>>m1>>m2>>m3;
-
-
       s[i]=student(roll,name,m1,m2,m3);
-
      }
-
-
-
      for(int i=0 ; i<3 ; i++)
      {
          cout<<endl<<"student "<<i+1<<"Details :"<<endl;
          s[i].display();
      }
-
-
-
      return 0;
  }
