9fce16506d917e0110aad1929b5fc4fa49351d59
DHRUVRAJSINH CHUDASAMA <chudasamadhruvraj8@gmail.com>
2025-04-20 10:01:47 +0530

    PRACTICAL2.1..cpp

--- PRACTICAL2.1..cpp
+++ PRACTICAL2.1..cpp
@@ -1,58 +1,44 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Rectangle
- {
+class Rectangle {
 private:
     int length;
     int width;
 
 public:
-
-    void setDimensions(int l, int w)
-    {
-
-            length = l;
-            width = w;
-
+    void setDimensions(int l, int w) {
+        length = l;
+        width = w;
     }
 
-
-    int area()
-     {
+    int area() {
         return length * width;
     }
 
-
-    int perimeter()
-     {
+    int perimeter() {
         return 2 * (length + width);
-     }
+    }
 };
 
-int main()
-
-{
+int main() {
     int n;
-    cout<<endl<<"enter for how many rectangles you want to calculate data";
-    cin>>n;
+    cout << "\nEnter how many rectangles you want to calculate data for: ";
+    cin >> n;
 
-    Rectangle r[n];
+    vector<Rectangle> r(n); 
 
- for(int i=0 ; i<n ; i++)
-{
+    for (int i = 0; i < n; i++) {
+        int l, w;
+        cout << "Enter length and width of rectangle " << i + 1 << ": ";
+        cin >> l >> w;
 
+        r[i].setDimensions(l, w);
 
-    int l, w;
-    cout << "Enter length and width of the rectangle: ";
-    cin >> l >> w;
+        cout << "Area of the rectangle: " << r[i].area() << endl;
+        cout << "Perimeter of the rectangle: " << r[i].perimeter() << endl;
+    }
 
-    r[i].setDimensions(l, w);
-
-    cout << "Area of the rectangle: " << r[i].area() << endl;
-    cout << "Perimeter of the rectangle: " << r[i].perimeter() << endl;
-
-
-}
     return 0;
 }
